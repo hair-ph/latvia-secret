d02fed88dd05ec3e8cf216743366c0649bc72e4b
hair-ph <78801252+hair-ph@users.noreply.github.com>
2021-03-26 14:27:51 +0200

    Add files via upload

---
+++ assets/js/translater.js
@@ -0,0 +1,190 @@
+var new_lang = {
+
+"lt0": "<i class=\"channel-icon\"></i>Izvēlne",
+
+"lt1": "<i class=\"channel-icon\"></i>Konferences",
+
+"lt2": "<i class=\"channel-icon\"></i>Diagnostika",
+
+"lt3": "<i class=\"channel-icon\"></i>Raksti",
+
+"lt4": "<i class=\"channel-icon\"></i>Vides apdrošināšana",
+
+"lt5": "<i class=\"channel-icon\"></i>Par mums", 
+
+"lt6": "Vai ir iespējams atgriezt redzi bez operācijas? Un kā novērst glaukomu un kataraktu?",
+
+"lt7": "Uz šiem un citiem svarīgiem jautājumiem atbild profesors, vispasaules oftalmoloģijas asociācijas pārstāvis Walter Ramsey.",
+
+"lt8": "Labdien, doktor. Šodien mēs gribam parunāt par to, kā saglabāt labu redzi un ko darīt, ja tā sāka pasliktināties.",
+
+"lt9": "Sveiki, labprāt atbildēšu uz visiem jūs interesējošiem jautājumiem un pastāstīšu par jaunākajiem sasniegumiem oftalmoloģijā.",
+
+"lt10": "Sakiet, lūdzu, vai daudziem cilvēkiem tagad ir problēmas ar redzi un kādas slimības ir sastopamas visbiežāk?",
+
+"lt11": "Šodien cilvēki ar ideālu redzi ir liels retums. Saskaņā ar PVO datiem, pasaulē apmēram 2 miljardi cilvēku cieš no acu slimībām. Turklāt pusē gadījumu slimības rodas profilakses trūkuma vai pienācīgas ārstēšanas.",
+
+"lt12": "Nopietnas problēmas sākas ar redzes pasliktināšanos. Ja ignorēt šo situāciju, iespējama viena no trim scenārijiem attīstīšanās: katarakta, glaukoma vai tīklenes centrālo nodaļu distrofija. Katarakta izpaužas ar acs lēcas apduļķošanos. Distrofijas gadījumā samazinās centrālā redze. Un gadījumā ar glaukomu – redzes zaudējums ir neatgriezenisks, jo aiziet bojā centrālais redzes nervs.",
+
+"lt13": "Es dzirdēju, ka glaukoma ir visbīstamākā slimība. Kas tas ir?",
+
+"lt14": "Kad sāk pasliktināties redze, jūs varat pat nezināt, ka attīstās glaukoma. Agrīnās stadijās to ir grūti diagnosticēt un tā var noritēt bez simptomiem. Visbiežāk glaukoma izpaužas, kad cilvēks zaudē redzeslauku. Mēdz būt tā, ka viena acs jau ir akla, bet cilvēks neievēroja un vēršas tad, kad vienīgā acs ir bēdīgā stāvoklī. Šodien glaukoma kļūst jaunāka, tāpat kā virkne citu patoloģiju, tādu kā, katarakta un tīklenes distrofija.",
+
+"lt15": "Kāpēc jauniešiem redze sāk bojāties?",
+
+"lt16": "Faktoru ir ļoti daudz. Protams, daļēji mūsu veselību nosaka ģenētika, tomēr vairumā gadījumu mūsu dzīvesveids noved pie problēmas rašanās.",
+
+"lt17": "Kāda ārstēšana tagad tiek praktizēta visbiežāk?",
+
+"lt18": "To ir trīs: konservatīvā (medikamentozā), ķirurģiskā un lāzera. Ārstēšanas taktikas izvēli nosaka slimības veids. Parasti pat glaukomu var ārstēt medikamentozi, samazinot intraokulāro spiedienu, uzlabojot asinsriti un normalizējot metabolismu acs ābola audos.",
+
+"lt19": "Tas ir, var iztikt bez operācijas un lāzera korekcijas?",
+
+"lt20": "Cilvēkiem ar tuvredzību un tālredzību, kuriem ir sākušas attīstīties acu saslimšanas, medikamentozi paaugstina peptīdus. Tā var ne tikai izvairīties no tādām nopietnām sekām, kā distrofijas, kataraktas un glaukomas, bet arī atgūt agrāk zaudēto redzi.",
+
+"lt21": "Pastāstiet, lūdzu, sīkāk.",
+
+"lt22": "Peptīdi spēj pastiprināt acs tīkleni par 50-90% un atveseļo visu acu orgāns. Tie sastāv no īsām aminoskābju ķēdītēm un darbojas kā regulators šūnu līmenī.",
+
+"lt23": "Un kur tie saturas?",
+
+"lt24": "Viens no populārākajiem un labi parādījušiem sevi preparātiem ar augstu peptīdu saturu es aktīvi izmantoju savā praksē – <a href=\"\">XX</a>. Tas nostiprina tīklenes un asinsvadu šūnu struktūru, normalizē olbaltumvielu biosintēzi un pozitīvi ietekmē genoma ekspresiju. Nonākot organismā, tas palīdz nodot signālu no DNS olbaltumvielām. Pateicoties tam, informācija no DNS sasniedz šūnas un sākas olbaltumvielu atveseļošanas process, kapilāru nostiprināšana un vispārēja acu atveseļošanās.",
+
+"lt25": "Tas ir, cilvēki ar sliktu redzi var reāli to atgūt?",
+
+"lt26": "Vairumā gadījumu-jā. Pārbaudot preparātu <a href=\"\">XX</a> tika veikts liels klīnisko pētījumu skaits, iesaistot pacientus ar tīklenes un acs radzenes deģeneratīvajiem procesiem.",
+
+"lt27": "Dalībniekiem bija sūdzības par redzes pasliktināšanos, distrofiskie procesi un acs ābolu asinsvadu darbības traucējumi. Dažiem no viņiem tika atzīmētas progresējošas patoloģiskas izmaiņas visos redzes orgānu nodalījumos. Uzsākot ārstēšanas kursu ar preparātu <a href=\"\">XX</a>, efekts bija novērojams 98% pacientu. Turklāt neatkarīgi no saslimšanas smaguma un veida. Preparāts palīdzēja uzlabot rādītājus gan cilvēkiem ar tuvredzību, gan tālredzību.",
+
+"lt28": "Un kādus rezultātus parāda jūsu prakse?",
+
+"lt29": "Izrakstot saviem pacientiem <a href=\"\">XX</a>, pēc ārstēšanas kursa katram ir novērojama pozitīva dinamika tīklenes rādītājos, tātad – redzes uzlabošanai. Pie kam neatkarīgi no hipermetropijas vai miopijas diagnozes.",
+
+"lt30": "Tāpat es atzīmēju blefarospazma, gaismas nepanesības un asarošanas izzušanu. Pēc viena kursa ar preparātu <a href=\"\">XX</a>, pacientiem notiek perifērās redzes paplašināšanās un redzesloka neredzīgo zonu samazināšanās vai izzušana.",
+
+"lt31": "Kādos acu saslimšanu gadījumos ir efektīvs preparāts?",
+
+"lt32": "Tam ir diezgan plašs darbības spektrs, jo acu muskuļi tonizējas. Tā kursa pielietošana vienlaikus atrisina vairākas būtiskas problēmas:",
+
+"lt33": "tiek noņemts saspringums, iekaisums, kairinājums un nogurums no acīm;",
+
+"lt34": "tiek noņemts sausās acs sindroms;",
+
+"lt35": "normalizējas intraokulārais spiediens;",
+
+"lt36": "paaugstinās redzes asums;",
+
+"lt37": "tiek novērsta acu slimību rašanās.",
+
+"lt38": "Vai preparātam ir kontrindikācijas?",
+
+"lt39": "<a href=\"\">XX</a> neizraisa imunitātes reakciju, tāpēc tā lietošana ir droša alerģiskiem cilvēkiem. Tāpat viņš neizraisa blakusparādības un zāļu atkarību.",
+
+"lt40": "Kur var iegādāties <a href=\"\">XX</a> ?",
+
+"lt41": "Diemžēl, preparātu ir diezgan grūti atrast brīvā pārdošanā. Visbiežāk to var iegādāties privātās klīnikās un aptiekās. Bet augstās efektivitātes dēļ <a href=\"\">XX</a> momentāli izpērk uzreiz pēc piegādes. Daudzi tīkli pastāvīgi paaugstina cenas, tādēļ es ieteiktu iegādāties <a href=\"\">XX</a> tieši no ražotāja. Vēl jo vairāk tāpēc, ka tieši no ražošanas tiek veikta piegāde uz jebkuru pilsētu, bez uzcenojumiem, kas ir ļoti ērti.",
+
+"lt42": "Liels paldies par interviju! Es domāju, ka daudzi cilvēki spēja atrast sev daudz noderīgu informāciju.",
+
+"lt43": "Un jums paldies par uzaicinājumu. Ceru, ka es detalizēti un saprotami atklāju visas jūs interesējošās tēmas.",
+
+"lt44": "Jums ir laiks<b>pasūtīt XX</b>. Atlaide pārstās darboties pēc<span class=\"counter-minutes\">09</span>:<span class=\"counter-seconds\">49</span>",
+
+"lt45": "Izvēlieties valsti:",
+
+"lt46": "Grieķija",
+
+"lt47": "Valsts 1",
+
+"lt48": "Valsts 2",
+
+"lt49": "Lūdzu, ievadiet vārdu:",
+
+"lt50": "Ievadiet tālruni:",
+
+"lt51": "Pasūtīt tagad ar atlaidi",
+
+"lt52": "Komentāri",
+
+"lt53": "Amēlija Rīvza",
+
+"lt55": "Paldies par interviju, bija ļoti interesanti izlasīt profesionāļa viedokli par šo nopietno problēmu.",
+
+"lt56": "Bonija Kārsone",
+
+"lt58": "Ja jūsu situācijai nav nepieciešama steidzama ķirurģiska iejaukšanās – mēģiniet <a href=\"\">XX</a>. Šeit ir dabīgs sastāvs, un iedarbojas ļoti ātri.",
+
+"lt59": "Roberts",
+
+"lt61": "Par redzi nekad nesūdzējos. Tiklīdz sāku sliktāk redzēt, nekavējoties devos pie ārsta. Nostādīja rindā uz redzes korekciju. Bet līdz tai izrakstīja dzert <a href=\"\">XX</a>. Kad es pēc mēneša atnācu uz pieņemšanu, tad ārsti ieraudzīja progresu. Un es ari pats sapratu, ka redze atgriežas. Kopumā, operācija tika atcelta, izrakstīja vingrošanu acīm un teica dzert <a href=\"\">XX</a> vēl vienu mēnesi. Kopumā pēc 2 mēnešiem mana redze kļuva ideāla, lai gan līdz tam bija +4,25 labā acs un 3,75 kreisā. Tā ka iesaku.",
+
+"lt62": "Hanna Meltone",
+
+"lt64": "Liels paldies par ieteikumu, Šodien pasūtīšu sev <a href=\"\">XX</a>, savādāk esmu jau nogurusi staigāt ar brillēm.",
+
+"lt65": "Anabella Belle",
+
+"lt67": "Es neticu, ka tādi līdzekļi darbojas!",
+
+"lt68": "Daniels Pūle",
+
+"lt70": "Es arī agrāk neticēju. Man bija darba acs trauma, kuras dēļ redze sāka kristies. Ilgu laiku man nekas nelīdzēja, operāciju nevarēju atļauties finansiālu iemeslu dēļ. Galu galā man palīdzēja XX, par kuru es paskatījos raidījumu vienā medicīnas kanālā. Izdzēru kursu, un pie manis atgriezās laba redze.",
+
+"lt71": "Skaidrīte",
+
+"lt73": "Cena neliela, bet pēc efektivitātes pagaidām labāku neesmu redzējusi līdzekli.",
+
+"lt74": "Estere Līvene",
+
+"lt76": "Es nosūtīju šo rakstu savam draugam-ārstam, un viņš teica, ka sastāvs ir ļoti labs, iesaka iegādāties.",
+
+"lt77": "Nellija",
+
+"lt79": "Priekš manis <a href=\"\">XX</a> ir vienīgais līdzeklis, kas palīdzēja atgūt redzi pēc traumas. Bet tā kā viss draudēja ar operāciju…",
+
+"lt80": "Rūta",
+
+"lt82": "Es vienmēr baidījos zaudēt redzi, lai gan vienmēr bija normāla redze. Un, lūk, pirms gada, neizskaidrojamu iemeslu dēļ, tā sāka samazināties. Es biju tik šokēta, ka man sāka notikt panikas lēkmes. Tas bija briesmīgi. Es baidījos veikt operāciju, jo ir ļoti liels risks, palikt aklam uz visiem laikiem. Es zinu, ka medicīna ir tālu attīstījusies, bet vienalga bija bail. Paziņa, zinot, cik es esmu bailīga, ieteica <a href=\"\">XX</a>. Tā sastāvā es neko neatradu, kas mani atbaidītu. Palasīju atsauksmes, pakonsultējos pie ārstiem – viņi visu apstiprināja. Un, kāds brīnums, man par laimi <a href=\"\">XX</a> palīdzēja! Mana redze kļuva aizvien labāka un labāka ar katru dienu, kamēr neatjaunojās līdz galam.",
+
+"lt83": "Vēlaties rakstīt ziņojumu?",
+
+"lt84": "Nosūtīt",
+
+"lt85": "BLOGI",
+
+"lt86": "Deivs Fornels, DAIC redaktors",
+
+"lt87": "8 veidi atgūt redzes asumu",
+
+"lt88": "Larss Markuss, medicīnas zinātņu doktors",
+
+"lt89": "Vitamīni, kas nepieciešami acīm",
+
+"lt90": "Linda D. Gillama, medicīnas zinātņu doktore, veselības aprūpes maģistre",
+
+"lt91": "Brilles bojā redzi",
+
+"lt92": "Ieraudzīt visus blogus",
+
+"lt93": "Zināšanu Marketing Term Injection",
+
+"lt94": "Cardiovascular Business,Cardiovascular Education,Cath Lab",
+
+"lt95": "Kontakti",
+
+"lt96": "Intervijas",
+
+"lt97": "Par mums",
+
+
+};
+function Translater () {
+ for (class_name in new_lang) {
+ var elements = document.getElementsByClassName(class_name);
+ if (elements.length) {
+ for (key in elements) {
+ elements[key].innerHTML = new_lang[class_name];
+ }
+ }
+ }
+ };
